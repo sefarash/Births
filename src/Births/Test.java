8d2943f105ba6df52238bcab21929567ac993c6c
Rashad Ismayilov <ismayilov.rashad@gmail.com>
2018-06-09 16:36:06 -0500

    Add test

---
+++ src/Births/Test.java
@@ -0,0 +1,15 @@
+package Births;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 20;
+		int c = ++b;
+		System.out.println(a++ + ++b + c);
+
+	}
+
+}
